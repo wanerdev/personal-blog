c51572eaded01ac243355fdd0eb151e8dcb2d16b
wanerkevin <wanerkevin@ordenador-portatil-de-waner-2.home>
2025-01-11 13:28:38 +0100

    Modify Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://www.wanerdelana.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
